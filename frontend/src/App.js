840604dc3a1994b155c42d2e8aa949119e3b37f1
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-08-13 17:21:29 -0500

    Removed warninings and fixed className

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
@@ -11,13 +10,13 @@ function App() {
     <div className="App">
       <Sidebar/>
 
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col min-vh-100 p-4">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col min-vh-100 p-4">
             
-              <button class="btn float-end" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" role="button">
+              <button className="btn float-end" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" >
                 hello
-                  <i class="bi bi-arrow-right-square-fill fs-3" data-bs-toggle="offcanvas" data-bs-target="#offcanvas"></i>
+                  <i className="bi bi-arrow-right-square-fill fs-3" data-bs-toggle="offcanvas" data-bs-target="#offcanvas"></i>
               </button>
               <h3>Sample Content...</h3>
               <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
