90778ef7f4e001cd6b6036b660484cfbf513d4e3
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-08-13 17:52:24 -0500

    Added Arrow Icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,8 +15,7 @@ function App() {
           <div className="col min-vh-100 p-4">
             
               <button className="btn float-end" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" >
-                hello
-                  <i className="bi bi-arrow-right-square-fill fs-3" data-bs-toggle="offcanvas" data-bs-target="#offcanvas"></i>
+                <span className="material-symbols-outlined">arrow_forward_ios</span>
               </button>
               <h3>Sample Content...</h3>
               <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
