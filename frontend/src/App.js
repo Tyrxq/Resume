acf6da14ba8b1dac7f26c48a4479dbe1c66cb6aa
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-08-29 20:05:05 -0500

    Added a contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Home from './Pages/Home';
 import Resume from './Pages/Resume';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/resume" element={<Resume/>} />
+          <Route path="/contact" element={<Contact/>} />
+          
         </Routes>
       </Router>
       
