81831f44d076ffb4d5f6814bfe30ca5f0dc18fc6
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-09-30 19:46:36 -0500

    Organized files for sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+import Sidebar from './components/Sidebar/Sidebar';
 import Home from './Pages/Home';
 import Resume from './Pages/Resume';
 import Contact from './Pages/Contact';
 import Portfolio from './Pages/Portfolio';
-import SidebarButton from './components/SidebarButton';
+import SidebarButton from './components/Sidebar/SidebarButton';
 
 function App() {
   return (
