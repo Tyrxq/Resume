01cc1994f4a4ba9a86d6fd514b89fcadc545c748
TyriqDo <tyriqdominguez@gmail.com>
2024-08-12 16:18:18 -0500

    Added rotates inward function

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,12 +1,37 @@
-import React, { useEffect } from 'react';
+import React  from 'react';
 import { useRef, useState } from 'react';
 import {Canvas, useFrame} from "@react-three/fiber";
 import {  OrbitControls } from '@react-three/drei';
 
 
-const Cube = ({position, color, rotate, setRotate,opacity}) => {
+const Cube = ({position, color, rotate, setRotate,opacity,id}) => {
   const meshRef = useRef(null);
   const [currentRotation, setCurrentRotation] = useState(0);
+
+  const rotateInward = () => {
+    setCurrentRotation(currentRotation+1);
+    if(id === 0 || id === 7){
+      meshRef.current.rotation.x -= Math.PI/180;
+      meshRef.current.rotation.z += Math.PI/180;
+      meshRef.current.rotation.y -= Math.PI/180;
+    }
+    else if(id === 2|| id === 5){
+      meshRef.current.rotation.x += Math.PI/180;
+      meshRef.current.rotation.z -= Math.PI/180;
+      meshRef.current.rotation.y -= Math.PI/180;
+      
+    }
+    else if(id === 3 || id === 6 ){
+      meshRef.current.rotation.x -= Math.PI/180;
+      meshRef.current.rotation.z -= Math.PI/180;
+      meshRef.current.rotation.y += Math.PI/180;
+    }
+    else if(id === 4 || id === 1){
+      meshRef.current.rotation.x += Math.PI/180;
+      meshRef.current.rotation.z += Math.PI/180;
+      meshRef.current.rotation.y += Math.PI/180;
+    }
+  }
   
 
   useFrame(() => {
@@ -14,15 +39,16 @@ const Cube = ({position, color, rotate, setRotate,opacity}) => {
       return;
     }
   if(rotate){
-    //const delta = meshRef.current.position.y + 0.0001;
-    //console.log(meshRef.current.rotation.x);
-    meshRef.current.rotation.x += Math.PI/90;
-    setCurrentRotation(currentRotation+2);
-    //console.log(currentRotation % (Math.PI/2));
-    //meshRef.current.rotation.y += 0.01;
-    //meshRef.current.position.y = 7 * Math.sin(delta) + 3
+    rotateInward()
   }
-  if(currentRotation % 90 === 0 ){
+  if(currentRotation % 90 === 0){
+    meshRef.current.rotation.x = 0;
+    meshRef.current.rotation.z = 0;
+    meshRef.current.rotation.y = 0;
+    
+  }
+  if(currentRotation === 180){
+    setCurrentRotation(0);
     setRotate(false);
   }
    
@@ -44,14 +70,16 @@ const CubeMatrix = ({position,color,space, opacity}) => {
   const [rotate,setRotate] = useState(false);
   return(
     <group>
-      <Cube position ={position} color={color} rotate={rotate} setRotate={setRotate} opacity = {opacity}/>
-      <Cube position={[x,y+space,z]} color={color} rotate={rotate} setRotate={setRotate} opacity = {opacity}/>
-      <Cube position={[x+space,y,z]} color={color} rotate={rotate} setRotate={setRotate} opacity = {opacity}/>
-      <Cube position={[x,y,z+space]} color={color} rotate={rotate} setRotate={setRotate} opacity = {opacity}/>
-      <Cube position={[x+space,y,z+space]} color={color} rotate={rotate} setRotate={setRotate} opacity = {opacity}/>
-      <Cube position={[x+space,y+space,z+space]} color={color} rotate={rotate} setRotate={setRotate} opacity = {opacity}/>
-      <Cube position={[x+space,y+space,z]} color={color} rotate={rotate} setRotate={setRotate} opacity = {opacity}/>
-      <Cube position={[x,y+space,z+space]} color={color} rotate={rotate} setRotate={setRotate} opacity = {opacity}/>
+      <Cube position ={position} color={'red'} rotate={rotate} setRotate={setRotate} opacity = {opacity} id = {0}/>
+      <Cube position={[x,y,z+space]} color={'purple'} rotate={rotate} setRotate={setRotate} opacity = {opacity} id = {1}/>
+      <Cube position={[x+space,y,z+space]} color={'blue'} rotate={rotate} setRotate={setRotate} opacity = {opacity} id = {2} />
+      <Cube position={[x+space,y,z]} color={'green'} rotate={rotate} setRotate={setRotate} opacity = {opacity} id = {3}/>
+      <Cube position={[x+space,y+space,z]} color={'white'} rotate={rotate} setRotate={setRotate} opacity = {opacity} id = {4}/>
+      <Cube position={[x,y+space,z]} color={'yellow'} rotate={rotate} setRotate={setRotate} opacity = {opacity}  id = {5}/>
+      <Cube position={[x,y+space,z+space]} color={color} rotate={rotate} setRotate={setRotate} opacity = {opacity} id = {6}/>
+      <Cube position={[x+space,y+space,z+space]} color={'pink'} rotate={rotate} setRotate={setRotate} opacity = {opacity} id = {7}/>
+     
+      
     </group>
   )
 }
@@ -65,7 +93,7 @@ const Home = () => {
           <pointLight position={[10,0,30]}/>
           <OrbitControls/>
           <group>
-            <CubeMatrix position={[0,0,0]} space={2} color={'rgba(255, 255, 255)'} opacity={.2}/>
+            <CubeMatrix position={[0,0,0]} space={2} color={'black'} opacity={1}/>
           </group>
           
        </Canvas>
