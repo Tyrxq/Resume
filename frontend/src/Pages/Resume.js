b9c15fed5d1a4e7a8274dbebeb7aa73cc5eb912f
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2024-12-05 18:48:43 -0600

    Added Contact Component

--- frontend/src/Pages/Resume.js
+++ frontend/src/Pages/Resume.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from '../components/Home/Home';
 import Experience from '../components/Experience/Experience';
 import Projects from '../components/Projects/Projects';
+import Contact from '../components/Contact/Contact';
 
 
 
@@ -13,10 +14,8 @@ const Resume = ({contactRef,portfolioRef,homeRef,resumeRef}) => {
               <Home homeRef={homeRef}/>
               <Experience experienceRef={resumeRef}/>
               <Projects projectsRef = {portfolioRef}/>
-                <div ref={contactRef} className='pt-5' id='contact'>
-                  <h3>Contact</h3>
-                </div>
-                <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies, forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+              <Contact contactRef={contactRef}/>
+                
             </div>
         </div>
     </div>
