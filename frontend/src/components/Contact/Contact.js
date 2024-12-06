b9e0b4d72b663162ac0a3d32b5f832120944c4af
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2024-12-06 03:03:10 -0600

    Added the input fields

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css'
 
 const Contact = ({contactRef}) => {
   return (
@@ -6,10 +7,22 @@ const Contact = ({contactRef}) => {
       <div ref={contactRef} id='contact' className='d-flex justify-content-center'>
         <h3 className='pt-5 display-2'>Contact</h3>
       </div>
-      <hr className=' line'></hr>
-      <div>
-      <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies, forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+      <hr className='line'></hr>
+      <div className='pt-5'>
+        <h5 className='display-5'>Feel free to leave a message!</h5>
+        <h5  className='display-5'>Thank you for viewing my website!</h5>
       </div>
+      <div class="input-group mb-3 mt-5 d-flex justify-content-center">
+        <span className="input-group-text" id="basic-addon1">Optional</span>
+        <input className='w-50' type="text" class="form-control" placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1"/>
+      </div>
+      <div className='d-flex justify-content-center pb-2 w-100'>
+        <textarea className='w-75 form-control' id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+      <div className='pb-3'>
+        <button type="button" class="btn btn-primary">Submit</button>
+      </div>
+      
     </div>
   )
 }
