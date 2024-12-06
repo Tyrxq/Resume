73b55475d01f6dff1469cec7f15f44c74bf1507d
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2024-12-06 03:22:08 -0600

    Added Placehoulder for textarea

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -9,7 +9,6 @@ const Contact = ({contactRef}) => {
       </div>
       <hr className='line'></hr>
       <div className='pt-5'>
-        <h5 className='display-5'>Feel free to leave a message!</h5>
         <h5  className='display-5'>Thank you for viewing my website!</h5>
       </div>
       <div class="input-group mb-3 mt-5 d-flex justify-content-center">
@@ -17,7 +16,7 @@ const Contact = ({contactRef}) => {
         <input className='w-50' type="text" class="form-control" placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1"/>
       </div>
       <div className='d-flex justify-content-center pb-2 w-100'>
-        <textarea className='w-75 form-control' id="exampleFormControlTextarea1" rows="3"></textarea>
+        <textarea className='w-75 form-control' id="exampleFormControlTextarea1" rows="3" placeholder="Feel free to leave a message!"></textarea>
       </div>
       <div className='pb-3'>
         <button type="button" class="btn btn-primary">Submit</button>
