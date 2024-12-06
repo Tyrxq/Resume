9b20cc2c9252692d4cb229dae37061fc8f65f484
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2024-12-06 07:05:37 -0600

    Fixed email input length error

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -13,7 +13,7 @@ const Contact = ({contactRef}) => {
       </div>
       <div class="input-group mb-3 mt-5 d-flex justify-content-center">
         <span className="input-group-text" id="basic-addon1">Optional</span>
-        <input className='w-50' type="text" class="form-control" placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1"/>
+        <input className='w-50 d-flex' type="text"  placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1"/>
       </div>
       <div className='d-flex justify-content-center pb-2 w-100'>
         <textarea className='w-75 form-control' id="exampleFormControlTextarea1" rows="3" placeholder="Feel free to leave a message!"></textarea>
