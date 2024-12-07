495d40902cd994a6e446e48782f3161ac9e64d61
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2024-12-07 01:51:55 -0600

    Added sending emails feature

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import  React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './Contact.css'
 
 const Contact = ({contactRef}) => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(process.env.REACT_APP_SERVICE_ID ,process.env.REACT_APP_TEMPLATE_ID , form.current, {
+        publicKey: process.env.REACT_APP_EMAIL_KEY,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+
+
+
   return (
     <div>
       <div ref={contactRef} id='contact' className='d-flex justify-content-center'>
@@ -11,16 +35,21 @@ const Contact = ({contactRef}) => {
       <div className='pt-5'>
         <h5  className='display-5'>Thank you for viewing my website!</h5>
       </div>
-      <div class="input-group mb-3 mt-5 d-flex justify-content-center">
-        <span className="input-group-text" id="basic-addon1">Optional</span>
-        <input className='w-50 d-flex' type="text"  placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1"/>
-      </div>
-      <div className='d-flex justify-content-center pb-2 w-100'>
-        <textarea className='w-75 form-control' id="exampleFormControlTextarea1" rows="3" placeholder="Feel free to leave a message!"></textarea>
-      </div>
-      <div className='pb-3'>
-        <button type="button" class="btn btn-primary">Submit</button>
-      </div>
+      <form ref={form} onSubmit={sendEmail}>
+        <div class="input-group mb-3 mt-5 d-flex justify-content-center">
+          <span className="input-group-text" id="basic-addon1">Optional</span>
+          <input className='w-50 d-flex' type="email" name='email' placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1"/>
+        </div>
+        <div className='d-flex justify-content-center pb-2 w-100'>
+          <textarea className='w-75 form-control' name='message' id="exampleFormControlTextarea1" rows="3" placeholder="Feel free to leave a message!"></textarea>
+        </div>
+        <div className='pb-3'>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </form>
+     
+     
+      
       
     </div>
   )
